8ed0265cf5b004047aa3a3d6c7d702f7e8a129e4
grace ngure <wnguregrace@gmail.com>
2024-07-02 13:04:11 +0300

    featlab complete

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response ,jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -17,7 +17,46 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+    def get(self):
+     response_dict={
+        "message":"Welcome to to the Newsletter RESTful API",
+     }
 
+     return make_response(response_dict ,200)
+    
+api.add_resource(Home ,'/')
+       
+class Newsletters(Resource):
+    def get(self):
+     response_dict=[n.to_dict() for n in Newsletter.query.all()]
+
+     return make_response(response_dict ,200)
+    
+   
+    def post(self):
+     new_record = Newsletter(
+        title=request.form['title'],
+        body=request.form['body'],
+    )
+
+     db.session.add(new_record)
+     db.session.commit()
+
+     response_dict = new_record.to_dict()
+
+     response = make_response(
+        response_dict,
+        201,
+    )
+
+     return response
+     
+api.add_resource(Newsletters ,'/newsletters')
+
+class NewslettersById(Resource):
+   def get(self,id):
+      response_dict= Newsletter.query.filter_by(id=id).first().to_dict()
+      return make_response(response_dict ,200)
+api.add_resource(NewslettersById,'/newsletters/<int:id>')   
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
